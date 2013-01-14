2c49536a69770496bd50cfd56b2dcba7d17ba308
Robert Pyke <robert.j.pyke@gmail.com>
2013-01-14 11:50:44 +1000

    Added new rails app to repo. Rails 3.2.11

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webapp::Application.config.secret_token = '70f7bf4477d340379f9b61854f00aa0e382b02b8c556d801eb6c6d6fcea708aea5c27e822f32c7e7df6f2831f315657afc3ae3a55235e371b984ee294c4df97e'
